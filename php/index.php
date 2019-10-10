ff0db8db3533aaecebf000bf45fcfc36c707a7c2
ulisesTerrazas <ulises.alumno.sc@gmail.com>
2019-10-09 20:30:39 -0500

    archivo php

--- php/index.php
+++ php/index.php
@@ -4,5 +4,22 @@
     Mas de una linea
 
 */
+ $variableOne = 100;
+ $variableTwo = 100;
 
+ $result = $variableOne + $variableTwo;
+
+ $result = $variableOne - $variableTwo;
+ $result = $variableOne * $variableTwo;
+ $result = $variableOne / $variableTwo;
+ print $result;
+
+ if ($result != 300) {
+     echo 'success';
+ }else{
+    echo 'unsuccess';
+ }
+
+
+ 
 ?>
